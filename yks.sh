2c0a983a820c3c0950154a88f96bcec59efbb848
Metincloup <88655361+Metincloup@users.noreply.github.com>
2024-05-01 20:57:33 +0300

    xfce comaptible

--- yks.sh
+++ yks.sh
@@ -15,5 +15,10 @@ remaining_days=$((remaining_seconds / 86400))  # 1 gün = 86400 saniye
 
 python3 YKS-Timer-Walpaper-main/script.py "$remaining_days"
 
+if [ $(echo $DESKTOP_SESSION) = 'xfce' ]; then
+# xfce için duvar kağıdını ayarlayın
+	xfconf-query -c xfce4-desktop -p $(xfconf-query -c xfce4-desktop -l | grep "workspace0/last-image") -s "$download_folder/image.jpg"
+else
 # Nitrogen ile duvar kağıdını ayarlayın
 feh --bg-scale "wallpaper.png"
+fi
